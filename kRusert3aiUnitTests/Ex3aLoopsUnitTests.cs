72f2ef9722dab67d0375b5d5e49dd4320e482e32
Keegan <Keegan@DESKTOP-2RO8Q56>
2022-11-28 23:16:52 -0600

    Add project files.

---
+++ kRusert3aiUnitTests/Ex3aLoopsUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Ex3A;
+
+namespace kRusert3aiUnitTests
+{
+    [TestClass]
+    public class Ex3aLoopsUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //don't have access
+        }
+    }
+}
---
+++ kRusert3aiUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("kRusert3aiUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kRusert3aiUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0131b395-5e11-4a59-bbbb-e161c67db26b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
